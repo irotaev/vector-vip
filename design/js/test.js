fe10a7e1735f189b52c1259202e2aa2b862fb0cc
Andrey <irotaev@gmail.com>
2013-05-20 01:10:57 +0400

    design mark 20.05.2013

--- design/js/test.js
+++ design/js/test.js
@@ -346,10 +346,15 @@ $(document).ready(function() {
     VVL.Scrolling_M.afterload();
 
     // Включаю плагин подсказок
-    $('#home-gallery-preview .gallery a').poshytip({
-        className: 'tip-darkgray',
-        bgImageFrameSize: 11,
-        offsetX: -25
+//    $('#home-gallery-preview .gallery a').poshytip({
+//        className: 'tip-darkgray',
+//        bgImageFrameSize: 11,
+//        offsetX: -25
+//    });
+
+    // Включаю плагин фотогалереи
+    $('.fancybox').fancybox({
+        scrollOutside: false
     });
 
     //Инициализурую битрикс панель
